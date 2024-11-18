8827ea314d5eebceb97ce4a3d71ba37f25b353bc
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-18 23:17:10 +0600

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+      int myNum = 88;               
+      float myFloatNum = 5.99f;    
+      char myLetter = 'D';          
+      boolean myBool = true;        
+      String myText = "Hi I ma Shajalal(Sojib)";      
+      System.out.println(myNum);
+      System.out.println(myFloatNum);
+      System.out.println(myLetter);
+      System.out.println(myBool);
+      System.out.println(myText);
+    }
+  }
+  
