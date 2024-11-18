b503ca4ce3046aad7c7fef0bfca069a0b364ca6f
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-18 23:06:26 +0600

    Create Multiple-Variables-Sum.java
    The two numble sum

---
+++ Multiple-Variables-Sum.java
@@ -0,0 +1,8 @@
+ public class Valu {
+ 
+    public static void main(String[] args) {
+        int FirstNumber = 20;
+        int SecondNumber = 55;
+        System.out.println(FirstNumber+SecondNumber);
+    }
+ }
