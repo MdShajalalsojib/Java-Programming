c860017c93872d6c1a8114b19958aa49d1b2732a
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-18 22:57:25 +0600

    Create Java Variables.java
    Real life Examplo

---
+++ Java Variables.java
@@ -0,0 +1,16 @@
+public class Valu {
+    public static void main(String[] args) {
+        String studentName = "Md.Shajalal";
+        int studentID = 2088;
+        int studentAge = 23;
+        float studentCgpa = 3.50;
+        char studentGrade = 'A';
+        
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentCgpa);
+        System.out.println("Student grade: " + studentGrade);
+      }
+    }
+    
