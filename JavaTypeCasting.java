de4ffc27cd80795ee6c65b162c9d8c8db7a299f7
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-19 21:31:19 +0600

    Create JavaTypeCasting.java

---
+++ JavaTypeCasting.java
@@ -0,0 +1,9 @@
+public class JavaTypeCasting {
+
+    public static void main(String[] args) {
+        int sumeNumber = 98;
+        double TheNumber = (double)sumeNumber;
+        System.out.println(sumeNumber);
+        System.out.println(TheNumber);
+    }
+}
