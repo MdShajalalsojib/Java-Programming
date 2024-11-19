bc0d5337b3e9cd72b9b0828d47c2062bf4a658ff
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-19 22:04:21 +0600

    Create ExaStringJava.java

---
+++ ExaStringJava.java
@@ -0,0 +1,11 @@
+public class string {
+    public static void main(String[] args) {
+        String tex = "Md.Shajalal";
+        String firsTex = "Java Programming";
+        String secondTex = "C Programming";
+        System.out.println("The length of the string tex"+tex);
+        System.out.println(firsTex+""+secondTex);
+        System.out.println(secondTex);
+    }
+    
+}
