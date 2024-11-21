7a9bce1ca2ecbf7245bce4a04f2b99e7e94c8103
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-21 13:21:29 +0600

    Create JavaIfSta.java
    Java If Statement

---
+++ JavaIfSta.java
@@ -0,0 +1,13 @@
+public class JavaIfSta {
+    public static void main(String[] args) {
+        int number = 20;
+        if (number<0){
+            System.out.println("The number is negative");
+
+        }
+        System.out.println("The number is progetive");
+    }
+
+}   
+---------------Code is output -------------------
+The number is progetive 
