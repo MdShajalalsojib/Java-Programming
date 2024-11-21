1379808b551106659158c9f711dc7031364cc9f8
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-21 13:36:00 +0600

    Create JavaIf...Else.java
    If.........Else

---
+++ JavaIf...Else.java
@@ -0,0 +1,27 @@
+----------------------------- if ....... Else---------------------
+    if (condition){
+           //Codes
+         }
+    else {
+      //codes
+      }
+Note: Mani will work if condition before working any condition and if that work is not if then else will work...
+----------------------if Else Example ------------------
+
+public class JavaIfSta {
+    public static void main(String[] args) {
+        int number = 20;
+        if (number<0){
+            System.out.println("The number is negative");
+
+        }
+        else{
+            System.out.println("The number is positive");
+        }
+        System.out.println("Statement outside if...else block");
+    }
+
+}   
+--------------Output---------------------
+The number is positive
+Statement outside if...else block
