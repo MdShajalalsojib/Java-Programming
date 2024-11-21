b0817fa958fccd36f130b91fbbacfab3e20d4f6e
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-21 13:04:04 +0600

    Create Java Booleans.java
    Java Boolens

---
+++ Java Booleans.java
@@ -0,0 +1,14 @@
+public class Booleans {
+
+    public static void main(String[] args) {
+        int myfirstNumber = 881;
+        int mysecondNumber = 91;
+        if (myfirstNumber >= mysecondNumber) {
+            System.out.println("Old enough to vote!");
+          } else {
+            System.out.println("Not old enough to vote.");
+          }  
+
+    }
+}
+----------The code of Output : Old enough to vote!-------------------
