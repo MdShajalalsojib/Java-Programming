83d07fff852ce9254eea09c1b7fef885ca0d3adc
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-24 22:24:37 +0600

    Create Java-do....while Loops.java

---
+++ Java-do....while Loops.java
@@ -0,0 +1,17 @@
+do {
+    // body of loop
+} while(textExpression);
+------------------------------------------------------------------------------
+ 
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+    int i = 1, n = 5;
+    // do...while loop from 1 to 5
+    do {
+      System.out.println(i);
+      i++;
+    } while(i <= n);
+  }
+}
